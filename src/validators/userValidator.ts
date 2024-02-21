1733e47f91bcefb096dc5b4e99ff88f4e985e03f
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-02-21 13:02:07 +0100

    fix

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,11 +1,11 @@
 import Joi, {LanguageMessages} from "joi";
 
-const mb = 500*1024
+const kb = 1024
 const userValidator = Joi.object({
     name: Joi.string().min(2).max(5).required(),
     age: Joi.number().min(16).max(20).required(),
     avatar: Joi.any().required().custom((value, helpers) => {
-        if (value && value[0].size > 2 * mb) {
+        if (value && value[0].size > 500 * kb) {
             return helpers.message('File size must be less or equal 500kb' as any as LanguageMessages)
         }
 
